10eda7be78e604ae1adbfd0920aa8b852629cc51
init <79251139+seyja@users.noreply.github.com>
2023-09-30 06:21:58 +0100

    Update server.js

--- multiplayer/server.js
+++ multiplayer/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const port = 3000;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, "public")));
 
